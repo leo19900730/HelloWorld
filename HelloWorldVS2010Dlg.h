3c8fa9cfb73682be609ae0f69f6f35682a067582
leo19900730 <liuzpzp007@163.com>
2017-04-01 11:23:42 +0800

    2017-04-01

---
+++ HelloWorldVS2010Dlg.h
@@ -0,0 +1,32 @@
+
+// HelloWorldVS2010Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// CHelloWorldVS2010Dlg 对话框
+class CHelloWorldVS2010Dlg : public CDialogEx
+{
+// 构造
+public:
+	CHelloWorldVS2010Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_HELLOWORLDVS2010_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
